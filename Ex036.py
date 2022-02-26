b6a25a83fb664f0d7f63296d703dc72fe7c5e8d5
paulosv2 <paulo_sv2008@hotmail.com>
2022-02-25 21:30:35 -0300

    Exercício 36

--- Ex036.py
+++ Ex036.py
@@ -3,11 +3,11 @@ casa = float(input('Insíra o valor do imóvel que deseja comprar: '))
 anos = int(input('Insíra em quantos anos deseja parcelar esse imóvel: '))
 
 valor_parcela = casa / anos / 12
-comprometido = valor_parcela * 100 / salario
+porcentagem = valor_parcela * 100 / salario
 
 if valor_parcela <= salario * 0.3:
-    print('O valor da sua parcela será de R${:.2f}! Com isso você estará comprometendo {:.1f}% do seu salário. Parabéns! Seu financiamento foi aprovado!'.format(valor_parcela, comprometido))
+    print('O valor da sua parcela será de R${:.2f}! Com isso você estará comprometendo {:.1f}% do seu salário. Parabéns! Seu financiamento foi aprovado!'.format(valor_parcela, porcentagem))
 else:
-    print('O valor da sua parcela será de R${:.2f}! Com isso você estará comprometendo {:.1f}% do seu salário. Infelizmente não podemos aprovar um financiamento para você nesse momento.'.format(valor_parcela, comprometido))
+    print('O valor da sua parcela será de R${:.2f}! Com isso você estará comprometendo {:.1f}% do seu salário. Infelizmente não podemos aprovar um financiamento para você nesse momento.'.format(valor_parcela, porcentagem))
 
 
